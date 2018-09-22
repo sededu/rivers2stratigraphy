799453569ae5c9cc32cf3297b9024102e3b5646e
amoodie <amoodie@rice.edu>
2018-09-22 13:12:30 -0500

    delete temp, change miniconda install approach

--- import_test.py
+++
@@ -1,1 +0,0 @@
-import rivers2stratigraphy
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -5,12 +5,6 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 
 from rivers2stratigraphy.channel import ActiveChannel
 
-def test1():
-    assert True
-
-def test2():
-    assert True
-
 def default_ActiveChannel():
     activeChannel = ActiveChannel()
     assert activeChannel.avul_num == 0
