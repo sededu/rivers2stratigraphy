2658dfc056feb93e7e1029b5abcf2aa613bcd55b
amoodie <amoodie@rice.edu>
2018-09-23 17:53:49 -0500

    add figure tests and baselines

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -9,24 +9,28 @@ import matplotlib
 
 
 def test_get_matplotlib_backend():
+
     ans = matplotlib.get_backend()
     platType = platform.system()
+
     if platType in {'Linux'}:
         assert ans == 'Qt5Agg'
+
     elif platType in {'Darwin'}:
         assert ans == 'MacOSX'
     
 
-# def test_set_matplotlib_backend():
-#     matplotlib.use('Qt5Agg', warn=False)
+def test_set_matplotlib_backend():
+    matplotlib.use('Qt5Agg', warn=False)
 
-#     ans = matplotlib.get_backend()
-#     assert ans == 'Qt5Agg'
+    ans = matplotlib.get_backend()
+    assert ans == 'Qt5Agg'
 
-@pytest.mark.mpl_image_compare(baseline_dir=os.path.join('figs_baseline'))
+
+@pytest.mark.mpl_image_compare(baseline_dir='figs_baseline')
 def test_launch_fig_compare():
-    from rivers2stratigraphy.gui import GUI, Runner
-    from rivers2stratigraphy.strat import Strat
+
+    from rivers2stratigraphy.gui import GUI
 
     gui = GUI()
     return gui.fig
