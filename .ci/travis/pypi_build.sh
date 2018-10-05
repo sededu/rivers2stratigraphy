17a677c80c911e4c79ec4fc997db3249c56383f9
amoodie <amoodie@rice.edu>
2018-10-05 16:38:12 -0500

    full test

--- .ci/travis/pypi_build.sh
+++ .ci/travis/pypi_build.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+# borrowed from Landlab implementation
+
+if [[ "$TRAVIS_TAG" == v* ]]; then
+  echo "Installing deployment requirements."
+  pip install twine wheel
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    echo "Creating a source distribution."
+    python setup.py sdist
+  else
+    echo "Creating a binary wheel distribution."
+    python setup.py bdist_wheel
+  fi
+else
+  echo "Not deploying."
+  echo "Tag is... $TRAVIS_TAG"
+  echo "Branch name is... $TRAVIS_BRANCH"
+fi
--- .ci/travis/pypi_upload.sh
+++ .ci/travis/pypi_upload.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+# borrowed from Landlab implementation
+
+if [[ "$TRAVIS_TAG" == v* ]]; then
+  echo "Uploading to Pypi."
+  twine upload -u amoodie -p$PYPI_PASS dist/*
+  echo "Done."
+else
+  echo "Not deploying."
+  echo "Tag is... $TRAVIS_TAG"
+  echo "Branch name is... $TRAVIS_BRANCH"
+fi
