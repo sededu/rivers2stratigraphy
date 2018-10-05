141c36616609893658e5061c85f1187ba7b3ae63
amoodie <amoodie@rice.edu>
2018-10-05 16:13:24 -0500

    try deploy

--- .ci/travis/anaconda_build.sh
+++ .ci/travis/anaconda_build.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
 
-conda install -q conda-build anaconda-client
-conda build .ci/conda-recipe/ --output-folder .ci/conda-build/
+{ # try
+
+    conda create -q -n build-environment python=$TRAVIS_PYTHON_VERSION
+	source activate build-environment
+
+} || { # catch
+
+    echo -e "Failed to create new build-environemnt!"
+    exit 1
+
+}
+
+
+{ # try
+
+    conda install -q conda-build anaconda-client
+	conda build -q .ci/conda-recipe/ --output-folder .ci/conda-build/ --no-test
+
+} || { # catch
+
+    echo -e "Failed during build in anaconda_build.sh!"
+    exit 1
+
+}
+
+
--- .ci/travis/anaconda_upload.sh
+++ .ci/travis/anaconda_upload.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Uploading to Anaconda Cloud"
-anaconda -t $CONDA_TOKEN upload --force --user sededu .ci/conda-build/**/rivers2stratigraphy*bz2
+{ # try
 
-echo "Done."
+	echo "Uploading to Anaconda Cloud"
+	anaconda -t $CONDA_TOKEN upload --force --user sededu .ci/conda-build/**/rivers2stratigraphy*bz2
+	echo "Upload to Anaconda Cloud successful!"
+
+} || { # catch
+
+	echo -e "Failed during upload to Anaconda Cloud!"
+	exit 1
+
+}
+
+
+
